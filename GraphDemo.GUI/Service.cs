e8b1482816816a7086a2736256e8ffc2cab141d8
Avi Avni <avi.avni@gmail.com>
2021-03-08 17:30:54 +0200

    update code

--- GraphDemo.GUI/Service.cs
+++ GraphDemo.GUI/Service.cs
@@ -1,9 +1,8 @@
 ﻿using Google.Maps;
-using Neo4j.Driver.V1;
+using Neo4j.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GraphDemo.GUI
@@ -16,9 +15,9 @@ namespace GraphDemo.GUI
         private const string DirectQuery = @"
 MATCH (s1:Stop)<-[:LOCATED_AT]-(st1:Stoptime)-[:PRECEDES*]->(st2:Stoptime)-[:LOCATED_AT]->(s2:Stop),
       (st1)-[:PART_OF_TRIP]->(t:Trip)-[:USES]->(r:Route)<-[:OPERATES]-(a:Agency)
-WHERE s1.id = {source} AND 
-      s2.id = {target} AND 
-      st1.arrival_time.hour = toInteger({time}) 
+WHERE s1.id = $source AND 
+      s2.id = $target AND 
+      st1.arrival_time.hour = toInteger($time) 
 RETURN [a, r, t, s1, st1, s2, st2] as nodes
 LIMIT 1";
 
@@ -27,9 +26,9 @@ LIMIT 1";
 MATCH (s1:Stop)<-[:LOCATED_AT]-(st1:Stoptime)-[:PRECEDES*]->(st2:Stoptime)-[:LOCATED_AT]->(s2:Stop)<-[:LOCATED_AT]-(st3:Stoptime)-[:PRECEDES*]->(st4:Stoptime)-[:LOCATED_AT]->(s3:Stop), 
       (st1)-[:PART_OF_TRIP]->(t:Trip)-[:USES]->(r:Route)<-[:OPERATES]-(a:Agency),
       (st3)-[:PART_OF_TRIP]->(t2:Trip)-[:USES]->(r2:Route)<-[:OPERATES]-(a2:Agency)
-WHERE s1.id = {source} AND 
-      s3.id = {target} AND 
-      st1.arrival_time.hour = toInteger({time}) AND 
+WHERE s1.id = $source AND 
+      s3.id = $target AND 
+      st1.arrival_time.hour = toInteger($time) AND 
       duration.inSeconds(st2.arrival_time, st3.arrival_time).seconds < 30*60 AND 
       st2.arrival_time < st3.arrival_time
 RETURN [a, r, t, s1, st1, s2, st2, a2, r2, t2, s2, st3, s3, st4] as nodes
@@ -41,10 +40,10 @@ MATCH (s1:Stop)<-[:LOCATED_AT]-(st1:Stoptime)-[:PRECEDES*]->(st2:Stoptime)-[:LOC
       (s3:Stop)<-[:LOCATED_AT]-(st3:Stoptime)-[:PRECEDES*]->(st4:Stoptime)-[:LOCATED_AT]->(s4:Stop), 
       (st1)-[:PART_OF_TRIP]->(t1:Trip)-[:USES]->(r1:Route)<-[:OPERATES]-(a1:Agency),
       (st3)-[:PART_OF_TRIP]->(t2:Trip)-[:USES]->(r2:Route)<-[:OPERATES]-(a2:Agency)
-WHERE s1.id = {source} AND 
-      s4.id = {target} AND 
+WHERE s1.id = $source AND 
+      s4.id = $target AND 
       distance(s2.location, s3.location) < 100 AND
-      st1.arrival_time.hour = toInteger({time}) AND 
+      st1.arrival_time.hour = toInteger($time) AND 
       duration.inSeconds(st2.arrival_time, st3.arrival_time).seconds < 30*60 AND 
       st2.arrival_time < st3.arrival_time
 RETURN [a1, r1, t1, s1, st1, s2, st2, a2, r2, t2, s3, st3, s4, st4] as nodes
@@ -53,7 +52,7 @@ LIMIT 1";
         public static Stop[] GetStops(string city, string street, bool isTrain)
         {
             var conditions = new List<string>();
-            if(!string.IsNullOrEmpty(city))
+            if (!string.IsNullOrEmpty(city))
             {
                 conditions.Add($"s.desc CONTAINS 'עיר: {city}'");
             }
@@ -66,24 +65,24 @@ LIMIT 1";
                 conditions.Add($"s.desc CONTAINS 'רחוב:{street}'");
             }
 
-            using (var session = driver.Session())
-            {
-                var query = $@"
+            var session = driver.Session();
+            var query = $@"
 MATCH (s:Stop) 
 WHERE {string.Join(" AND ", conditions)}
 RETURN s
 ORDER BY s.name";
-                return session.Run(query).Select(r =>
+
+            var records = session.Run(query).ToArray();
+            return records.Select(r =>
+            {
+                var node = r["s"].As<INode>();
+                return new Stop()
                 {
-                    var node = r["s"].As<INode>();
-                    return new Stop()
-                    {
-                        Id = node.Properties["id"].ToString(),
-                        Name = node.Properties["name"].ToString(),
-                        Desc = node.Properties["desc"].ToString()
-                    };
-                }).ToArray();
-            }
+                    Id = node.Properties["id"].ToString(),
+                    Name = node.Properties["name"].ToString(),
+                    Desc = node.Properties["desc"].ToString()
+                };
+            }).ToArray();
         }
 
         public static async Task<Response> CreatePlanAsync(string source, string target, string time, PlanType planType)
@@ -91,20 +90,18 @@ ORDER BY s.name";
             if (source == null || target == null || time == null)
                 return null;
 
-            using (var session = driver.Session())
+            var session = driver.AsyncSession();
+            var parameters = new Dictionary<string, object>()
             {
-                var parameters = new Dictionary<string, object>()
-                {
-                    [nameof(source)] = source,
-                    [nameof(target)] = target,
-                    [nameof(time)] = time
-                };
+                [nameof(source)] = source,
+                [nameof(target)] = target,
+                [nameof(time)] = time
+            };
 
-                return await RunQueryAsync(session, planType, parameters);
-            }
+            return await RunQueryAsync(session, planType, parameters);
         }
 
-        private static async Task<Response> RunQueryAsync(ISession session, PlanType planType, Dictionary<string, object> parameters)
+        private static async Task<Response> RunQueryAsync(IAsyncSession session, PlanType planType, Dictionary<string, object> parameters)
         {
             var query = GetQuery(planType);
             var result = await (await session.RunAsync(query, parameters)).ToListAsync();
--- GraphDemo/Program.cs
+++ GraphDemo/Program.cs
@@ -1,14 +1,12 @@
 ﻿using CsvHelper;
-using Neo4j.Driver.V1;
+using CsvHelper.Configuration;
+using Neo4j.Driver;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reactive.Disposables;
-using System.Reactive.Linq;
-using System.Reactive.Subjects;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -16,174 +14,150 @@ namespace GraphDemo
 {
     class Program
     {
-        const string BasePath = @"C:\israel-public-transportation";
+        const string BasePath = @"C:\Graph\israel-public-transportation";
         static IDriver driver;
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             driver = GraphDatabase.Driver("bolt://localhost", AuthTokens.Basic("neo4j", "123456"));
 
-            CreateSchema();
+            await CreateSchema();
 
-            Save("agency.txt",
-                 "CREATE (:Agency {id: toInteger({agency_id}), name: {agency_name}, url: {agency_url}, timezone: {agency_timezone}, lang: {agency_lang}})",
+            await Save("agency.txt",
+                 "UNWIND $data as item CREATE (:Agency {id: toInteger(item.agency_id), name: item.agency_name, url: item.agency_url, timezone: item.agency_timezone, lang: item.agency_lang})",
                  5000,
                  new Property("agency_id"),
-                 new Property("agency_name"), 
-                 new Property("agency_url"), 
+                 new Property("agency_name"),
+                 new Property("agency_url"),
                  new Property("agency_timezone"),
                  new Property("agency_lang"));
 
-            Save("routes.txt",
-                 "MATCH (a:Agency {id: toInteger({agency_id})}) CREATE (a)-[:OPERATES]->(r:Route {id: {route_id}, desc: {route_desc}, color: {route_color}, short_name: {route_short_name}, long_name: {route_long_name}, type: toInteger({route_type})})",
-                 5000,
-                 new Property("route_id"), 
-                 new Property("agency_id"), 
-                 new Property("route_short_name"), 
-                 new Property("route_long_name"), 
-                 new Property("route_desc"), 
-                 new Property("route_type"),
-                 new Property("route_color"));
+            await Save("routes.txt",
+                  "UNWIND $data as item MATCH (a:Agency {id: toInteger(item.agency_id)}) CREATE (a)-[:OPERATES]->(r:Route {id: item.route_id, desc: item.route_desc, color: item.route_color, short_name: item.route_short_name, long_name: item.route_long_name, type: toInteger(item.route_type)})",
+                  5000,
+                  new Property("route_id"),
+                  new Property("agency_id"),
+                  new Property("route_short_name"),
+                  new Property("route_long_name"),
+                  new Property("route_desc"),
+                  new Property("route_type"),
+                  new Property("route_color"));
 
-            Save("trips.txt",
-                 "MATCH (r:Route {id: {route_id}}) CREATE (r)<-[:USES]-(t:Trip {id: {trip_id}, service_id: {service_id}, headsign: {trip_headsign}, direction_id: {direction_id}, shape_id: {shape_id}})",
+            await Save("trips.txt",
+                 "UNWIND $data as item MATCH (r:Route {id: item.route_id}) CREATE (r)<-[:USES]-(t:Trip {id: item.trip_id, service_id: item.service_id, headsign: item.trip_headsign, direction_id: item.direction_id, shape_id: item.shape_id})",
                  5000,
-                 new Property("route_id"), 
-                 new Property("service_id"), 
-                 new Property("trip_id"), 
-                 new Property("trip_headsign"), 
+                 new Property("route_id"),
+                 new Property("service_id"),
+                 new Property("trip_id"),
+                 new Property("trip_headsign"),
                  new Property("direction_id"),
                  new Property("shape_id"));
 
-            Save("stops.txt",
-                 "CREATE (s:Stop {id: {stop_id}, name: {stop_name}, location: point({longitude: toFloat({stop_lon}), latitude: toFloat({stop_lat}), crs: 'wgs-84'}), parent_station: {parent_station}, location_type: {location_type}, code: {stop_code}, desc: {stop_desc}, zone: {zone_id}})",
+            await Save("stops.txt",
+                 "UNWIND $data as item CREATE (s:Stop {id: item.stop_id, name: item.stop_name, location: point({longitude: toFloat(item.stop_lon), latitude: toFloat(item.stop_lat), crs: 'wgs-84'}), parent_station: item.parent_station, location_type: item.location_type, code: item.stop_code, desc: item.stop_desc, zone: item.zone_id})",
                  5000,
-                 new Property("stop_id"), 
-                 new Property("stop_code"), 
-                 new Property("stop_name"), 
-                 new Property("stop_desc"), 
-                 new Property("stop_lat"), 
-                 new Property("stop_lon"), 
-                 new Property("location_type"), 
+                 new Property("stop_id"),
+                 new Property("stop_code"),
+                 new Property("stop_name"),
+                 new Property("stop_desc"),
+                 new Property("stop_lat"),
+                 new Property("stop_lon"),
+                 new Property("location_type"),
                  new Property("parent_station"),
                  new Property("zone_id"));
 
-            Save("stops.txt",
-                 "MATCH (ps:Stop {id: {parent_station}}), (s:Stop {id: {stop_id}}) CREATE (ps)<-[:PART_OF]-(s)",
+            await Save("stops.txt",
+                 "UNWIND $data as item MATCH (ps:Stop {id: item.parent_station}), (s:Stop {id: item.stop_id}) CREATE (ps)<-[:PART_OF]-(s)",
                  5000,
                  new Property("stop_id"),
                  new Property("parent_station", o => o != null));
 
-            Save("stop_times.txt",
-                 "MATCH (t:Trip {id: {trip_id}}), (s:Stop {id: {stop_id}}) CREATE (t)<-[:PART_OF_TRIP]-(st:Stoptime {arrival_time: localtime({arrival_time}), departure_time: localtime({departure_time}), stop_sequence: toInteger({stop_sequence}), pickup_type: {pickup_type}, drop_off_type: {drop_off_type}, shape_dist_traveled: {shape_dist_traveled}})-[:LOCATED_AT]->(s)",
+            await Save("stop_times.txt",
+                 "UNWIND $data as item MATCH (t:Trip {id: item.trip_id}), (s:Stop {id: item.stop_id}) CREATE (t)<-[:PART_OF_TRIP]-(st:Stoptime {arrival_time: localtime(item.arrival_time), departure_time: localtime(item.departure_time), stop_sequence: toInteger(item.stop_sequence), pickup_type: item.pickup_type, drop_off_type: item.drop_off_type, shape_dist_traveled: item.shape_dist_traveled})-[:LOCATED_AT]->(s)",
                  10000,
-                 new Property("trip_id"), 
-                 new Property("arrival_time", selector: FormatTime), 
-                 new Property("departure_time", selector: FormatTime), 
-                 new Property("stop_id"), 
-                 new Property("stop_sequence"), 
-                 new Property("pickup_type"), 
+                 new Property("trip_id"),
+                 new Property("arrival_time", selector: FormatTime),
+                 new Property("departure_time", selector: FormatTime),
+                 new Property("stop_id"),
+                 new Property("stop_sequence"),
+                 new Property("pickup_type"),
                  new Property("drop_off_type"),
                  new Property("shape_dist_traveled"));
 
-            Save("trips.txt",
-                 "MATCH (s1:Stoptime)-[:PART_OF_TRIP]->(t:Trip), (s2:Stoptime)-[:PART_OF_TRIP]->(t) WHERE t.id={trip_id} AND s2.stop_sequence=s1.stop_sequence+1 CREATE (s1)-[:PRECEDES]->(s2)",
-                 100,
+            await Save("trips.txt",
+                 "UNWIND $data as item MATCH (s1:Stoptime)-[:PART_OF_TRIP]->(t:Trip), (s2:Stoptime)-[:PART_OF_TRIP]->(t) WHERE t.id=item.trip_id AND s2.stop_sequence=s1.stop_sequence+1 CREATE (s1)-[:PRECEDES]->(s2)",
+                 1000,
                  new Property("trip_id"));
         }
 
         public static object FormatTime(object o)
         {
             var hour = int.Parse(o.ToString().Split(':')[0]);
-            return hour >= 24 ? o.ToString().Replace($"{hour}:", $"{(hour - 24).ToString("00")}:") : o;
+            return hour >= 24 ? o.ToString().Replace($"{hour}:", $"{hour - 24:00}:") : o;
         }
 
-        private static void CreateSchema()
+        private static async Task CreateSchema()
         {
-            using (var session = driver.Session())
-            {
-                session.Run("CREATE CONSTRAINT ON (a:Agency) ASSERT a.id IS UNIQUE");
-                session.Run("CREATE CONSTRAINT ON (r:Route) ASSERT r.id IS UNIQUE");
-                session.Run("CREATE CONSTRAINT ON (t:Trip) ASSERT t.id IS UNIQUE");
-                session.Run("CREATE CONSTRAINT ON (s:Stop) ASSERT s.id IS UNIQUE");
-                session.Run("CREATE INDEX ON :Trip(service_id)");
-                session.Run("CREATE INDEX ON :Stoptime(stop_sequence)");
-                session.Run("CREATE INDEX ON :Stop(name)");
-                session.Run("CREATE INDEX ON :Stop(location)");
-            }
+            var session = driver.AsyncSession();
+            await session.RunAsync("CREATE CONSTRAINT ON (a:Agency) ASSERT a.id IS UNIQUE");
+            await session.RunAsync("CREATE CONSTRAINT ON (r:Route) ASSERT r.id IS UNIQUE");
+            await session.RunAsync("CREATE CONSTRAINT ON (t:Trip) ASSERT t.id IS UNIQUE");
+            await session.RunAsync("CREATE CONSTRAINT ON (s:Stop) ASSERT s.id IS UNIQUE");
+            await session.RunAsync("CREATE INDEX ON :Trip(service_id)");
+            await session.RunAsync("CREATE INDEX ON :Stoptime(stop_sequence)");
+            await session.RunAsync("CREATE INDEX ON :Stop(name)");
+            await session.RunAsync("CREATE INDEX ON :Stop(location)");
+            await session.CloseAsync();
         }
 
-        public static void Save(string file,
+        public static async Task Save(string file,
             string query,
             int batch,
             params Property[] properties)
         {
             var sw = Stopwatch.StartNew();
 
-            Validate(query, properties.Select(p => p.Name).ToArray());
+            var session = driver.AsyncSession();
 
-            using (var session = driver.Session())
-            {
-                Observable
-                    .Create<CsvReader>(obs => CreateStream(file, obs))
-                    .Select(csv => ReadRecord(properties, csv))
-                    .Where(record => properties.Where(p => p.Condition != null).All(p => p.Condition(record[p.Name])))
-                    .Buffer(batch)
-                    .Do(parameters => SaveBatch(query, parameters, session))
-                    .Scan(0, (acc, args) => acc + args.Count)
-                    .Do(records => PrintProgress(file, records, sw.Elapsed))
-                    .Wait();
+            await CreateStream(file, properties)
+                .Where(record => properties.Where(p => p.Condition != null).All(p => p.Condition(record[p.Name])))
+                .Buffer(batch)
+                .Do(parameters => SaveBatch(query, parameters, session))
+                .Scan(0, (acc, args) => acc + args.Count)
+                .ForEachAsync(records => PrintProgress(file, records, sw.Elapsed));
 
-                Console.WriteLine();
-            }
+            Console.WriteLine();
+
+            await session.CloseAsync();
         }
 
-        private static void Validate(string query, string[] fields)
+        private static async IAsyncEnumerable<Dictionary<string, object>> CreateStream(string file, Property[] properties)
         {
-            var ps = Regex.Matches(query, "{([^:]*?)}").OfType<Match>().Select(x => x.Groups[1].Value).ToList();
-            if (ps.Except(fields).Any()) throw new Exception(string.Join(", ", ps.Except(fields)));
-            if (fields.Except(ps).Any()) throw new Exception(string.Join(", ", fields.Except(ps)));
-        }
+            using var reader = new StreamReader(Path.Combine(BasePath, file));
+            using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ",", Mode = CsvMode.NoEscape });
 
-        private static IDisposable CreateStream(string file, IObserver<CsvReader> obs)
-        {
-            using (var reader = new StreamReader(Path.Combine(BasePath, file)))
-            using (var csv = new CsvReader(reader))
+            await csv.ReadAsync();
+            csv.ReadHeader();
+
+            while (await csv.ReadAsync())
             {
-                csv.Configuration.IgnoreQuotes = true;
-                csv.Read();
-                csv.ReadHeader();
-
-                while (csv.Read())
+                var parameters = new Dictionary<string, object>(properties.Length);
+                foreach (var property in properties)
                 {
-                    obs.OnNext(csv);
+                    parameters.Add(property.Name, property.Selector(csv.GetField(property.Name)));
                 }
-
-                obs.OnCompleted();
+                yield return parameters;
             }
-
-            return Disposable.Empty;
         }
 
-        private static Dictionary<string, object> ReadRecord(Property[] properties, CsvReader csv)
+        private static Task SaveBatch(string query, IList<Dictionary<string, object>> parameters, IAsyncSession session)
         {
-            var parameters = new Dictionary<string, object>(properties.Length);
-            foreach (var property in properties)
+            return session.WriteTransactionAsync(async transaction =>
             {
-                parameters.Add(property.Name, property.Selector(csv.GetField(property.Name)));
-            }
-
-            return parameters;
-        }
-
-        private static void SaveBatch(string query, IList<Dictionary<string, object>> parameters, ISession session)
-        {
-            session.WriteTransaction(transaction =>
-            {
-                foreach (var item in parameters)
+                await transaction.RunAsync(query, new Dictionary<string, object>
                 {
-                    transaction.Run(query, item);
-                }
+                    ["data"] = parameters
+                });
             });
         }
 
